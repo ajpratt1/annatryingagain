b453f210740549cc252a62afdf20b2e61a009827
ajpratt1 <ajacob10@pratt.edu>
2023-01-24 21:25:42 -0500

    anna's second try
    my commit

---
+++ week_2/cats.py
@@ -0,0 +1,34 @@
+#Pepper
+#Kitty
+#Audrey
+#Cooper
+#June
+#Robbie
+#Addie
+
+list_of_cats = ['pepper', 'kitty', 'cooper', 'audrey', 'june']
+
+Cats = {
+	'pepper': 'small',
+	'kitty': 'big',
+	'Cooper': 'big',
+	'Audrey': 'small',
+	'June': 'medium'
+}
+
+cat = [
+	{'name': 'pepper', 'size': 'small', 'age': 3 },
+	{'name': 'kitty', 'size': 'big', 'age': 9 },
+	{'name': 'cooper', 'size': 'big', 'age': 4},
+	{'name': 'audrey', 'size': 'small', 'age': 1},
+	{'name': 'june', 'size': 'medium', 'age': 3},
+]
+
+cat2 = {
+	'pepper': {'size': 'small', 'age': 3},
+	'kitty': {'size': 'big', 'age': 9},
+}
+
+print(cat[1]['size'])
+
+print(cat2['pepper']['size'])
