fbea5339a3b73e50237bd5a49381b55dc0d39561
ajpratt1 <ajacob10@pratt.edu>
2023-02-21 19:35:53 -0500

    gotthisone

--- week_5/van_gogh_search.py
+++ week_5/van_gogh_search.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-url = "https://collectionapi.metmuseum.org/public/collection/v1/object"
+url = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=van gogh&isOnView=true&hasImages=true"
 headers = {'Content-Type': 'application/json'}
-res = requests.get(https://collectionapi.metmuseum.org/public/collection/v1/search/artistOrCulture/gogh, headers=headers)
+res = requests.get(url, headers=headers)
 res_json = res.json()
 res_json = json.loads(res.text)
 print(json.dumps(res_json, indent=2))
